2b108c230899c5c38262f8eabd2e7ce8cda1c088
benny <benny.li@sapia.ai>
2025-06-05 19:31:01 +0800

    feat: test change

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const App: React.FC = () => {
           children: [
             {
               path: '/inventory/fabric-entry',
-              name: '布料单录入',
+              name: '布料单录入111testss',
               icon: <PlusCircleOutlined />,
             },
             {
